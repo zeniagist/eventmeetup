211845db4b53532eecaccffccbc2cdcf46a8ecc0
Zenia Gist <zeniagist@gmail.com>
2021-04-03 10:10:16 -0500

    updated getAccessToken handler

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -61,21 +61,15 @@ module.exports.getAuthURL = async () => {
 };
 
 module.exports.getAccessToken = async (event) => {
-  // The values used to instantiate the OAuthClient are at the top of the file
   const oAuth2Client = new google.auth.OAuth2(
     client_id,
     client_secret,
     redirect_uris[0]
   );
-  // Decode authorization code extracted from the URL query
+
   const code = decodeURIComponent(`${event.pathParameters.code}`);
 
   return new Promise((resolve, reject) => {
-    /**
-     *  Exchange authorization code for access token with a “callback” after the exchange,
-     *  The callback in this case is an arrow function with the results as parameters: “err” and “token.”
-     */
-
     oAuth2Client.getToken(code, (err, token) => {
       if (err) {
         return reject(err);
@@ -84,21 +78,71 @@ module.exports.getAccessToken = async (event) => {
     });
   })
     .then((token) => {
-      // Respond with OAuth token
       return {
         statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        },
         body: JSON.stringify(token),
       };
     })
     .catch((err) => {
-      // Handle error
       console.error(err);
       return {
         statusCode: 500,
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        },
         body: JSON.stringify(err),
       };
     });
 };
+
+// module.exports.getAccessToken = async (event) => {
+//   // The values used to instantiate the OAuthClient are at the top of the file
+//   const oAuth2Client = new google.auth.OAuth2(
+//     client_id,
+//     client_secret,
+//     redirect_uris[0]
+//   );
+//   // Decode authorization code extracted from the URL query
+//   const code = decodeURIComponent(`${event.pathParameters.code}`);
+
+//   return new Promise((resolve, reject) => {
+//     /**
+//      *  Exchange authorization code for access token with a “callback” after the exchange,
+//      *  The callback in this case is an arrow function with the results as parameters: “err” and “token.”
+//      */
+
+//     oAuth2Client.getToken(code, (err, token) => {
+//       if (err) {
+//         return reject(err);
+//       }
+//       return resolve(token);
+//     });
+//   })
+//     .then((token) => {
+//       // Respond with OAuth token
+//       return {
+//         statusCode: 200,
+//         headers: {
+//           'Access-Control-Allow-Origin': '*'
+//         },
+//         body: JSON.stringify(token),
+//       };
+//     })
+//     .catch((err) => {
+//       // Handle error
+//       console.error(err);
+//       return {
+//         statusCode: 500,
+//         headers: {
+//           'Access-Control-Allow-Origin': '*'
+//         },
+//         body: JSON.stringify(err),
+//       };
+//     });
+// };
 
 module.exports.getCalendarEvents = async (event) => {
   // The values used to instantiate the OAuthClient are at the top of the file
@@ -129,7 +173,7 @@ module.exports.getCalendarEvents = async (event) => {
       }
     );
   })
-    .then((token) => {
+    .then((results) => {
       // Respond with OAuth token
       return {
         statusCode: 200,
