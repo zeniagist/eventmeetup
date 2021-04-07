0f09faff628fff5f4302607be94bd6bb43dc7299
Zenia Gist <zeniagist@gmail.com>
2021-04-07 11:56:25 -0500

    added test for App.js

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from '../App';
+
+describe('<App /> component', () => {
+
+  test('render list of events', () => {
+    const AppWrapper = shallow(<App />);
+    expect(AppWrapper.find(EventList)).toHaveLength(1);
+  });
+
+});
