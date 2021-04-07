33ef58acdae5591acb7c2fc487c9716f07d4a8d1
Zenia Gist <zeniagist@gmail.com>
2021-04-07 12:35:11 -0500

    added 4 Event list items to be tested

--- src/EventList.js
+++ src/EventList.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import Event from './Event'
 
 class EventList extends Component {
   render() {
+    const { events } = this.props;
     return (
       <ul className='EventList'>
-
+        {events.map(event =>
+          <li key={event.id}>
+            <Event event={event} />
+          </li>
+        )}
       </ul>
     )
   }
