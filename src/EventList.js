9aa820c7e977079047ec76b8681306876eccee2e
Zenia Gist <zeniagist@gmail.com>
2021-04-07 22:11:14 -0500

    updated to have mockdata for text

--- src/EventList.js
+++ src/EventList.js
@@ -6,11 +6,11 @@ class EventList extends Component {
     const { events } = this.props;
     return (
       <ul className='EventList'>
-        {events.map(event =>
+        {events.map((event) => (
           <li key={event.id}>
             <Event event={event} />
           </li>
-        )}
+        ))}
       </ul>
     )
   }
