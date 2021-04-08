9368d1a42c5c2a337b67789a0e82f0aa5f362d94
Zenia Gist <zeniagist@gmail.com>
2021-04-07 20:18:15 -0500

    added and refactored CitySearch for changing city input test

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 
 class CitySearch extends Component {
+  state = {
+    query: '',
+  }
+
+  // update state of city in text input
+  handleInputChanged = (event) => {
+    const value = event.target.value;
+    this.setState({ query: value });
+  }
+
   render() {
     return (
       <div className='CitySearch'>
@@ -8,6 +18,8 @@ class CitySearch extends Component {
         <input
           type='text'
           className='city'
+          value={this.state.query}
+          onChange={this.handleInputChanged}
         />
         {/* list of suggested cities */}
         <ul className='suggestions'>
--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -15,4 +15,18 @@ describe('<CitySearch /> component', () => {
   test('renders a list of suggested cities', () => {
     expect(CitySearchWrapper.find('.city')).toHaveLength(1);
   });
+  // text input w/ Google Calendar MockData
+  test('render text input with Google Calendar MockData', () => {
+    const query = CitySearchWrapper.state('query');
+    expect(CitySearchWrapper.find('.city').prop('value')).toBe(query);
+  });
+  // change state when text input changes
+  test('change state when text input changes', () => {
+    CitySearchWrapper.setState({
+      query: 'Munich'
+    });
+    const eventObject = { target: { value: 'Berlin' } };
+    CitySearchWrapper.find('.city').simulate('change', eventObject);
+    expect(CitySearchWrapper.state('query')).toBe('Berlin');
+  });
 });
