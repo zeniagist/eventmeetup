c75433c081da04c2a8f23b45be8dbafc94311e79
Zenia Gist <zeniagist@gmail.com>
2021-04-13 15:57:43 -0500

    installed atatus integration monitoring

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -16,6 +17,11 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
 serviceWorkerRegistration.unregister();
+
+// config atatus
+atatus.config('e36665c8623b4b22a6de75bdff23169a').install();
+
+// atatus.notify(new Error('Test Atatus Setup'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
