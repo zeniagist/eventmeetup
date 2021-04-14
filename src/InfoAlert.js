370ac68271e4431c60f5e3e84ada16a0ae07042f
Zenia Gist <zeniagist@gmail.com>
2021-04-14 15:16:00 -0500

    updated manifest images

--- src/InfoAlert.js
+++ src/InfoAlert.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class InfoAlert extends Component {
   constructor(props) {
     super(props);
-    this.color = 'yellow';
+    this.color = 'blue';
   }
 
   getStyle = () => {
