f2e17f80d5b3e6f11a0dc961ae4957f16b4c2524
Zenia Gist <zeniagist@gmail.com>
2021-04-16 16:35:34 -0500

    added chart to measure number of events in each city

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import {
+  ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip
+} from 'recharts';
 import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
@@ -11,6 +14,17 @@ class App extends Component {
     events: [],
     locations: [],
   }
+
+  // get location
+  getData = () => {
+    const { locations, events } = this.state;
+    const data = locations.map((location) => {
+      const number = events.filter((event) => event.location === location).length
+      const city = location.split(' ').shift()
+      return { city, number };
+    })
+    return data;
+  };
 
   // update events
   updateEvents = (location, eventCount) => {
@@ -64,6 +78,21 @@ class App extends Component {
         <h1 className='eventTitle'>Event Meetup App</h1>
         <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
         <NumberOfEvents updateEvents={this.updateEvents} />
+        <h4>Events in each city</h4>
+
+        <ScatterChart
+          width={400}
+          height={400}
+          margin={{
+            top: 20, right: 20, bottom: 20, left: 20,
+          }}
+        >
+          <CartesianGrid />
+          <XAxis type="category" dataKey="city" name="city" />
+          <YAxis type="number" dataKey="number" name="number of events" allowDecimals={false} />
+          <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+          <Scatter data={this.getData()} fill="#8884d8" />
+        </ScatterChart>
         <EventList events={this.state.events} />
       </div>
     );
