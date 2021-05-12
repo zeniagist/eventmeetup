cf3e327cb4c64c8cf1eb63f56dd3094978420a8d
Zenia Gist <zeniagist@gmail.com>
2021-05-12 16:42:07 -0500

    setup login page

--- src/Login.jsx
+++ src/Login.jsx
@@ -4,21 +4,20 @@ function Login() {
     <div className="App">
       <h1>Welcome to the Meet app</h1>
       <h4>
-        Log in to see upcoming events around the world for
-        full-stack
-        developers
-      </h4>
+      Log in to see upcoming events around the world for full-stack developers </h4>
       <div className="button_cont" align="center">
         <div class="google-btn">
           <div class="google-icon-wrapper">
             <img
-              class="google-icon"
-              src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
-              alt="Google sign-in"
+            class="google-icon"
+            src="https://upload.wikimedia.org/wikipedia/commons/5/53/G
+            oogle_%22G%22_Logo.svg"
+            alt="Google sign-in"
             />
           </div>
           <a
-            href="https://0u461j2wzj.execute-api.us-east-2.amazonaws.com/dev/api/get-auth-url"
+            href="THE URL RETURNED FROM YOUR getAuth
+            ENDPOINT HERE"
             rel="nofollow noopener"
             class="btn-text"
           >
@@ -27,7 +26,7 @@ function Login() {
         </div>
       </div>
       <a
-        href="https://glenzy.github.io/meet/privacy.html"
+        href="https://zeniagist.github.io/eventmeetup/privacy.html"
         rel="nofollow noopener"
       >
         Privacy policy
